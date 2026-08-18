9e3e5032e41c6f74c0e231392dc4ae5de058fc99
Brianbru2022 <117004161+Brianbru2022@users.noreply.github.com>
2026-08-18 10:28:31 +0100

    Merge Victorian V3 base and production styles

---
+++ app/VictorianBoardV3Styles.ts
@@ -0,0 +1,5 @@
+import base from "./VictorianBoardV3.module.css";
+import production from "./VictorianBoardV3Production.module.css";
+
+const styles = { ...base, ...production };
+export default styles;
