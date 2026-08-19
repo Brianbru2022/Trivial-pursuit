b81663b96677435703a38a0766cc01167f68e65f
Brianbru2022 <117004161+Brianbru2022@users.noreply.github.com>
2026-08-19 08:37:46 +0100

    Switch live app to stable full-board camera build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Discovery3DPlayable from "./Discovery3DPlayable";
+import Discovery3DPlayableFixed from "./Discovery3DPlayableFixed";
 
 export default function Home(){
-  return <Discovery3DPlayable/>;
+  return <Discovery3DPlayableFixed/>;
 }
